0657f2d691619896a206c43af873b85f12be4cdf
daniel <danielddemissie@gmail.com>
2022-12-15 18:36:59 +0300

    fix: migrate to yarn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
+        hello
+      </button>
     </div>
   );
 }
